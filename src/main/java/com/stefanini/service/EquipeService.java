39a1367b86a3a396b122e543f7caa581e78c8454
gaoliveira1 <gabriel.adamski.oliveira@gmail.com>
2015-12-09 08:58:19 -0200

    Gabriel - Trabalhando em update e listar de EquipeService

--- src/main/java/com/stefanini/service/EquipeService.java
+++ src/main/java/com/stefanini/service/EquipeService.java
@@ -1,6 +1,8 @@
 package com.stefanini.service;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import com.stefanini.entidade.Equipe;
 import com.stefanini.util.JPAUtil;
@@ -18,6 +20,22 @@ public class EquipeService {
 	public void update(Equipe equipe) {
 		EntityManager manager = JPAUtil.getEntityManager();
 		manager.getTransaction().begin();
-
+		Query query = manager.createQuery("UPDATE SGR_EQUIPE SET REGISTRO_VALIDADE_FIM = :dataFim WHERE ID_EQUIPE = :idEquipe");
+		query.setParameter("idEquipe", equipe.getIdEquipe());
+		query.setParameter("dataFim", equipe.getRegistroValidadeFim());
+		Equipe novaEquipe = new Equipe();
+		novaEquipe.setNome(equipe.getNome());
+		novaEquipe.setRegistroValidadeInicio(equipe.getRegistroValidadeFim());
+		save(novaEquipe);
+		manager.close();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Equipe> listar(){
+		EntityManager manager = JPAUtil.getEntityManager();
+		Query query = manager.createNativeQuery("SELECT * FROM SGR_EQUIPE WHERE REGISTRO_VALIDADE_INICIO = null", Equipe.class);
+		List<Equipe> equipe = (List<Equipe>) query.getSingleResult();
+		manager.close();
+		return equipe;
 	}
 }
