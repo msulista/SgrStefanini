346448c2755d6fc9ec6d927d20585686cfcfc3c7
gaoliveira1 <gabriel.adamski.oliveira@gmail.com>
2015-12-09 09:46:00 -0200

    Gabriel - Criado metodos updade, listar, getById e arquivar em Equipe service

--- src/main/java/com/stefanini/service/EquipeService.java
+++ src/main/java/com/stefanini/service/EquipeService.java
@@ -1,4 +1,5 @@
 package com.stefanini.service;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -20,22 +21,52 @@ public class EquipeService {
 	public void update(Equipe equipe) {
 		EntityManager manager = JPAUtil.getEntityManager();
 		manager.getTransaction().begin();
-		Query query = manager.createQuery("UPDATE SGR_EQUIPE SET REGISTRO_VALIDADE_FIM = :dataFim WHERE ID_EQUIPE = :idEquipe");
-		query.setParameter("idEquipe", equipe.getIdEquipe());
-		query.setParameter("dataFim", equipe.getRegistroValidadeFim());
+		
+		equipe.setRegistroValidadeFim(new Date());
 		Equipe novaEquipe = new Equipe();
 		novaEquipe.setNome(equipe.getNome());
 		novaEquipe.setRegistroValidadeInicio(equipe.getRegistroValidadeFim());
-		save(novaEquipe);
+		manager.persist(novaEquipe);
+		
+		Query query = manager.createNativeQuery("UPDATE SGR_EQUIPE SET REGISTRO_VALIDADE_FIM = :dataFim WHERE ID_EQUIPE = :id");
+		query.setParameter("dataFim", equipe.getRegistroValidadeFim());
+		query.setParameter("idEquipe", equipe.getIdEquipe());
+		query.executeUpdate();
+		
+		manager.getTransaction().commit();
 		manager.close();
 	}
 	
 	@SuppressWarnings("unchecked")
 	public List<Equipe> listar(){
 		EntityManager manager = JPAUtil.getEntityManager();
-		Query query = manager.createNativeQuery("SELECT * FROM SGR_EQUIPE WHERE REGISTRO_VALIDADE_INICIO = null", Equipe.class);
-		List<Equipe> equipe = (List<Equipe>) query.getSingleResult();
+		Query query = manager.createNativeQuery("SELECT * FROM SGR_EQUIPE WHERE REGISTRO_VALIDADE_INICIO <> null", Equipe.class);
+		List<Equipe> equipes = query.getResultList();
+		manager.close();
+		return equipes;
+	}
+	
+	public Equipe getEquipeById(long id){
+		EntityManager manager = JPAUtil.getEntityManager();
+		Query query = manager.createNativeQuery("SELECT * FROM SGR_EQUIPE WHERE ID_EQUIPE = :idEquipe", Equipe.class);
+		query.setParameter("idEquipe", id);
+		Equipe equipe = (Equipe) query.getSingleResult();
 		manager.close();
 		return equipe;
+	}
+	
+	public void arquivar(long id){
+		EntityManager manager = JPAUtil.getEntityManager();
+		Equipe equipe = getEquipeById(id);
+		equipe.setRegistroValidadeFim(new Date());
+		manager.getTransaction().begin();
+		
+		Query query = manager.createQuery("UPDATE SGR_EQUIPE SET REGISTRO_VALIDADE_FIM = :dataFim WHERE ID_EQUIPE = :id");
+		query.setParameter("dataFim", equipe.getRegistroValidadeFim());
+		query.setParameter("id", equipe.getIdEquipe());
+		query.executeUpdate();
+		manager.getTransaction().commit();
+		manager.close();
+		
 	}
 }
