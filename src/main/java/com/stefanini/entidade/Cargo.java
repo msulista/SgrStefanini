cf8385e6a091b67baa4938fcbc02ae15e51eb252
msulista <marcus.rodrigues81@gmail.com>
2015-12-11 09:16:19 -0200

    Marcus - Refatorando Cargo

--- src/main/java/com/stefanini/entidade/Cargo.java
+++ src/main/java/com/stefanini/entidade/Cargo.java
@@ -26,7 +26,11 @@ public class Cargo {
 	private Date registroValidadeFim;
 	
 	@Transient
-	private Date dataManipulacaoFim = new Date();
+	private Date dataManipulacaoFim;
+	
+	public Cargo() {
+		this.dataManipulacaoFim = new Date();
+	}
 
 	public long getId() {
 		return id;
