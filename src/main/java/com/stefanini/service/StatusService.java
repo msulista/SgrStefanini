c9102a6883479d34b3892641d274c1a0cc35cde8
gaoliveira1 <gabriel.adamski.oliveira@gmail.com>
2015-12-14 14:46:49 -0200

    Gabriel - adicionado verificação de dia util em crud Status

--- src/main/java/com/stefanini/service/StatusService.java
+++ src/main/java/com/stefanini/service/StatusService.java
@@ -1,6 +1,7 @@
 package com.stefanini.service;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import javax.faces.convert.ConverterException;
@@ -9,57 +10,87 @@ import javax.persistence.Query;
 
 import com.stefanini.entidade.Status;
 import com.stefanini.util.JPAUtil;
+import com.stefanini.util.Mensagem;
 
 public class StatusService {
 
-	public void save(Status status){
+	public boolean save(Status status) {
 		EntityManager manager = JPAUtil.getEntityManager();
 		manager.getTransaction().begin();
-		manager.persist(status);
-		manager.getTransaction().commit();
-		manager.close();
+		if (verificaDiaUtil(status.getRegistroValidadeInicio())) {
+			manager.persist(status);
+			manager.getTransaction().commit();
+			manager.close();
+			return true;
+		} else {
+			Mensagem.add("Data informada não é um dia util!");
+			manager.close();
+			return false;
+		}
 	}
-	
-	public void update(Status status){	
+
+	public boolean update(Status status) {
 		EntityManager manager = JPAUtil.getEntityManager();
-		manager.getTransaction().begin();	
-		
-		Status statusMerge = (Status)getStatusById(status.getId());
-		statusMerge.setRegistroValidadeFim(status.getDataManipulacao());
-		manager.merge(statusMerge);
-		manager.getTransaction().commit();
-		manager.close();
-		
-		Status statusPersist = new Status();
-		statusPersist.setNome(status.getNome());
-		statusPersist.setRegistroValidadeInicio(status.getDataManipulacao());		
-		save(statusPersist);
+		manager.getTransaction().begin();
+
+		if (verificaDiaUtil(status.getDataManipulacao())) {
+			Status statusMerge = (Status) getStatusById(status.getId());
+			statusMerge.setRegistroValidadeFim(status.getDataManipulacao());
+			manager.merge(statusMerge);
+			manager.getTransaction().commit();
+			manager.close();
+
+			Status statusPersist = new Status();
+			statusPersist.setNome(status.getNome());
+			statusPersist.setRegistroValidadeInicio(status.getDataManipulacao());
+			save(statusPersist);
+			return true;
+		} else {
+			Mensagem.add("Data informada não é um dia util!");
+			manager.close();
+			return false;
+		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public List<Status> listarAtivos(){
+	public List<Status> listarAtivos() {
 		EntityManager manager = JPAUtil.getEntityManager();
-		Query q = manager.createNativeQuery("SELECT * FROM sgr_status WHERE REGISTRO_VALIDADE_FIM IS NULL ORDER BY REGISTRO_VALIDADE_INICIO ASC", Status.class);
+		Query q = manager.createNativeQuery(
+				"SELECT * FROM sgr_status WHERE REGISTRO_VALIDADE_FIM IS NULL ORDER BY REGISTRO_VALIDADE_INICIO ASC",
+				Status.class);
 		List<Status> listStatus = q.getResultList();
 		return listStatus;
 	}
-	
-	public Status getStatusById(Long id){
+
+	public Status getStatusById(Long id) {
 		EntityManager manager = JPAUtil.getEntityManager();
 		Query q = manager.createNativeQuery("SELECT * FROM sgr_status WHERE ID_STATUS = :idStatus", Status.class);
 		q.setParameter("idStatus", id);
-		Status status = (Status)q.getSingleResult();
+		Status status = (Status) q.getSingleResult();
 		manager.close();
 		return status;
 	}
 
-	public void desativar(Long id) throws ConverterException{
-		EntityManager manager = JPAUtil.getEntityManager();	
-		manager.getTransaction().begin();		
-		Status statusMerge = (Status)getStatusById(id);
+	public void desativar(Long id) throws ConverterException {
+		EntityManager manager = JPAUtil.getEntityManager();
+		manager.getTransaction().begin();
+		Status statusMerge = (Status) getStatusById(id);
 		statusMerge.setRegistroValidadeFim(new Date());
 		manager.merge(statusMerge);
 		manager.getTransaction().commit();
-		manager.close();		
+		manager.close();
+	}
+
+	public boolean verificaDiaUtil(Date data) {
+		GregorianCalendar calendar = new GregorianCalendar();
+
+		calendar.setTime(data);
+
+		if (calendar.get(GregorianCalendar.DAY_OF_WEEK) == 1 || calendar.get(GregorianCalendar.DAY_OF_WEEK) == 7) {
+			return false;
+		} else {
+			return true;
+
+		}
 	}
 }
