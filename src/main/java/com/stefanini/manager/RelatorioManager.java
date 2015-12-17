6be7613ea1120689294e72f0315c5d36dcdbc314
msulista <marcus.rodrigues81@gmail.com>
2015-12-17 15:10:22 -0200

    Marcus - Refatorando tentando corrigir bug

--- src/main/java/com/stefanini/manager/RelatorioManager.java
+++ src/main/java/com/stefanini/manager/RelatorioManager.java
@@ -6,7 +6,10 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
 
 import com.ocpsoft.pretty.faces.annotation.URLMapping;
 import com.ocpsoft.pretty.faces.annotation.URLMappings;
@@ -22,11 +25,11 @@ public class RelatorioManager {
 	
 	private Relatorio relatorio = new Relatorio();
 	private RelatorioService service = new RelatorioService();
-	private BarChartModel profissionalPorEquipe = service.getProfissionalPorEquipe();
+	private BarChartModel profissionalPorEquipe;
 	
-	 @PostConstruct
+	@PostConstruct
 	public void init() {
-	   this.service.criaGrafico();
+	   criaGrafico();
 	}
 	
 	public Relatorio getRelatorio() {
@@ -50,5 +53,35 @@ public class RelatorioManager {
 	public List<Relatorio> relatorioProfissionalPorEquipe(){
 		return this.service.profissionaisPorEquipe();
 	}
+	    
+	    public void criaGrafico(){
+	    	createBarModel();
+	    }
+	    private BarChartModel initBarModel() {
+	        BarChartModel model = new BarChartModel();
+	 
+	        ChartSeries grafico = new ChartSeries();
+	        for (Relatorio relatorio : this.service.profissionaisPorEquipe()) {
+				grafico.set(relatorio.getNome(), relatorio.getQuantidade());
+			}	 
+	        model.addSeries(grafico);
+	         
+	        return model;
+	    }
+	     
+	    private void createBarModel() {
+	        profissionalPorEquipe = initBarModel();
+	         
+	        profissionalPorEquipe.setTitle("Profissionais por equipe");
+	        profissionalPorEquipe.setLegendPosition("ne");
+	         
+	        Axis xAxis = profissionalPorEquipe.getAxis(AxisType.X);
+	        xAxis.setLabel("Equipes");
+	         
+	        Axis yAxis = profissionalPorEquipe.getAxis(AxisType.Y);
+	        yAxis.setLabel("N° Profissionais");
+	        yAxis.setMin(0);
+	        yAxis.setMax(50);
+	    }
 
 }
