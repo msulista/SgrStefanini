2290bd4a21cafb138061f593abcfc2daadfb4d85
mateusss <mateus_silva_sousa@hotmail.com>
2015-12-17 10:45:23 -0200

    Mateus - Refatorado Mascaras

---
+++ src/main/java/com/stefanini/converter/MoedaConverter.java
@@ -0,0 +1,51 @@
+package com.stefanini.converter;
+
+import java.text.DecimalFormat;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter(value="moedaConverter") 
+public class MoedaConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component,
+			String value) throws ConverterException {
+		if (value != null && !value.equals("")) {
+			String cargaHoraria = value.replaceAll("\\,", "\\.");
+			try {
+				double cargaHorariaFormatada = Double.parseDouble(cargaHoraria);
+				return cargaHorariaFormatada;
+			} catch (NumberFormatException e) {
+				FacesMessage message = new FacesMessage(
+						FacesMessage.SEVERITY_ERROR, "Erro de conversão de Carga Horária",
+						"O valor informado não é um número de Carga Horária!");
+				throw new ConverterException(message);
+			}
+		} else {			
+			return null;
+		}
+	}
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+    	
+    	DecimalFormat df = new DecimalFormat("#.00");
+    	double numero = Double.parseDouble(value.toString());
+    	String numeroFormatado = df.format(numero);
+ 	
+    	
+        if (numeroFormatado != null && numeroFormatado.length() == 4){
+        	
+        	numeroFormatado = "0"+numeroFormatado;
+        	return numeroFormatado;
+        }
+        
+        return numeroFormatado;
+    }
+
+}
