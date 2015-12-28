dd11ee0728fd0795745aade2d28fbda183603a0d
msulista <marcus.rodrigues81@gmail.com>
2015-12-28 16:45:00 -0200

    Marcus - Refatorando crinado metodo busca por porofissionais por nome de equipe

--- src/main/java/com/stefanini/service/RelatorioService.java
+++ src/main/java/com/stefanini/service/RelatorioService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
+import com.stefanini.entidade.Profissional;
 import com.stefanini.entidade.Relatorio;
 import com.stefanini.util.JPAUtil;
 
@@ -21,5 +22,14 @@ public class RelatorioService {
 		
 		List<Relatorio> relatorioProfissionalPorEquipe = (List<Relatorio>)q.getResultList();
 		return relatorioProfissionalPorEquipe;		
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Profissional> listaDeProfissionaisPorEquipe(String nome){
+		EntityManager manager = JPAUtil.getEntityManager();
+		Query q = manager.createNamedQuery("Profissional.findProfissionalByEquipeNome");
+		q.setParameter("nome", nome);
+		List<Profissional> profissionais = q.getResultList();
+		return profissionais;
 	}
 }
